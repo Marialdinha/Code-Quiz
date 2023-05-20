28b5dd8bdf44b17bc5d845b18a94d45365b7bba0
Marialdinha <marialda@bellsouth.net>
2023-05-19 21:59:04 -0500

    Added questions and answers

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,49 @@
-var timerEl = document.getElementById("countdown");
-var explanation = document.getElementById("quiz-explain")
-var question1 = document.getElementById("question-nav1")
+// Creatimg questions 
+var myQuestions = [
+	{
+		question: "Inside which HTML element do we put the JavaScript?",
+		answers: {
+			a: "scripting",
+			b: "javascript",
+			c: "script",
+      d: "js",
 
+		},
+		correctAnswer: 'c'
+	},
+	{
+		question: "Where is the correct place to insert a JavaScript?",
+		answers: {
+			a: 'header',
+			b: 'body',
+			c: 'head',
+      d: 'head and body'
+		},
+		correctAnswer: 'b'
+	},
+  {
+		question: "How do you create a function in JavaScript?",
+		answers: {
+			a: 'function myFunction()',
+			b: 'function = myFunction()',
+			c: 'function: myFunction()',
+      d: 'var function(myFunction)'
+		},
+		correctAnswer: 'a'
+	}
+];
+
+// Defining variables
+var timerEl = document.getElementById("countdown");
+var explanation = document.getElementById("quiz-explain");
+var questionNav = document.getElementById("question-nav1");
+var question = document.getElementById("question");
+var Answer1 = document.getElementById("Answer1");
+var Answer2 = document.getElementById("Answer2");
+var Answer3 = document.getElementById("Answer3");
+var Answer4 = document.getElementById("Answer4");
+
+// Function Countdown
 function countdown() {
     var timeLeft = 51;
   
@@ -19,6 +61,7 @@ function countdown() {
       if(timeLeft === 0) { 
         clearInterval(timeInterval);
         timerEl.textContent = "Time is up";
+
         // Calling Time Is Up function
         timeIsUp()
        }
@@ -26,10 +69,16 @@ function countdown() {
     },1000);
   }
   
+  // Function StartQuiz
   function startQuiz(){
     countdown();
     explanation.style.display="none";
-    question1.style.display="block";
+
+    for (var i = 0; i <= myQuestions.length - 1; i++) {
+      question.textContent = myQuestions{i}
+     }
+
+    questionNav.style.display="block";
   }
 
   function timeIsUp(){
