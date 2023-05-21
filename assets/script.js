e41e99c654590f740f42bceffd17f6a2b9c274f6
Marialdinha <marialda@bellsouth.net>
2023-05-21 10:48:06 -0500

    checkcheckRightAnswer is workng correctly

--- assets/script.js
+++ assets/script.js
@@ -8,27 +8,27 @@ var myQuestions = [
 			3: "script",
       4: "js"
 		},
-		correctAnswer: '3'
+		correctAnswer: 3
 	},
 	{
 		question: "Where is the correct place to insert a JavaScript?",
 		answers: {
-			1: 'header',
-			2: 'body',
-			3: 'head',
-      4: 'head and body'
+			1: "header",
+			2: "body",
+			3: "head",
+      4: "head and body"
 		},
-		correctAnswer: '2'
+		correctAnswer: 2
 	},
   {
 		question: "How do you create a function in JavaScript?",
 		answers: {
-			1: 'function myFunction()',
-			2: 'function = myFunction()',
-			3: 'function: myFunction()',
-      4: 'var function(myFunction)'
+			1: "function myFunction()",
+			2: "function = myFunction()",
+			3: "function: myFunction()",
+      4: "var function(myFunction)"
 		},
-		correctAnswer: '1'
+		correctAnswer: 1
 	}
 ];
 
@@ -37,17 +37,18 @@ var timerEl = document.getElementById("countdown");
 var explanation = document.getElementById("quiz-explain");
 var questionNav = document.getElementById("question-nav1");
 var questionContainer = document.getElementById("question_container");
-var answer1Container = document.getElementById("answer1_container");
-var answer2Container = document.getElementById("answer2_container");
-var answer3Container = document.getElementById("answer3_container");
-var answer4Container = document.getElementById("answer4_container");
+var answer1Btn = document.getElementById("answer1_btn");
+var answer2Btn = document.getElementById("answer2_btn");
+var answer3Btn = document.getElementById("answer3_btn");
+var answer4Btn = document.getElementById("answer4_btn");
 var writeWrong = document.getElementById("write-wrong");
 var quizEnd = document.getElementById("quiz-end");
 var startBtn = document.getElementById("start");
 var nextBtn = document.getElementById("next");
-var isDisplayingQuestion = false
+var answerNumber = 0;
 var timeLeft = 51;
 var i = 0;
+
 
 
 // INITIALIZATION
@@ -66,7 +67,7 @@ function countdown() {
 
       timeLeft--;
       // Displaying time left
-      timerEl.textContent = "Timer: 00:" 
+      timerEl.textContent = "Timer: 00:";
       if (timeLeft < 10) {
         timerEl.textContent =  timerEl.textContent + "0" + timeLeft;
       } else{
@@ -78,7 +79,7 @@ function countdown() {
         timerEl.textContent = "Time is up";
 
         // Calling Time Is Up function
-        timeIsUp()
+        timeIsUp();
        }
        
     },1000);
@@ -88,51 +89,79 @@ function countdown() {
 function startQuiz(){
     countdown();
     explanation.style.display="none";
-    questionNav.style.display="block";
-    start.style.display="none";
+    questionNav.style.display="initial";
+    startBtn.style.display="none";
 
        // DISPLAYING THE QUESTIONS
-      i = 0;
-      writeWrong.style.display="none";
-      if (i < myQuestions.length && timeLeft >0) {
-       
-         diplayQuestion()
-
-         //add event listener for answers
-         answer1Container.addEventListener("click", checkRightAnswer);  
-         answer2Container.addEventListener("click", checkRightAnswer); 
-         answer3Container.addEventListener("click", checkRightAnswer); 
-         answer4Container.addEventListener("click", checkRightAnswer); 
-         i++;
-        }else{
-          next.style.display="none";
-        }
+      i = -1;
+      displayQuestion()
+     
  
 }
+// DISPLAY QUESITON FUNCTION
+function displayQuestion(){
+  i++;
+     if (i < myQuestions.length) {
 
-function diplayQuestion(){
-        questionContainer.textContent = myQuestions[i].question
-        answer1Container.textContent = myQuestions[i].answers["1"]
-        answer2Container.textContent = myQuestions[i].answers["2"]
-        answer3Container.textContent = myQuestions[i].answers["3"]
-        answer4Container.textContent = myQuestions[i].answers["4"]
+        writeWrong.style.display="none";
+        nextBtn.style.display="none";
+        answer1Btn.disabled = false;
+        answer2Btn.disabled = false;
+        answer3Btn.disabled = false;
+        answer4Btn.disabled = false;
+
+        questionContainer.textContent = myQuestions[i].question;
+        answer1Btn.textContent = myQuestions[i].answers["1"];
+        answer2Btn.textContent = myQuestions[i].answers["2"];
+        answer3Btn.textContent = myQuestions[i].answers["3"];
+        answer4Btn.textContent = myQuestions[i].answers["4"];
+      } else{
+        timeLeft = 0;
+      }
 }
 
-function checkRightAnswer(answerNumber){
+function Btn1Pessed(){
+  answerNumber = 1;
+  checkRightAnswer()
+}
+
+function Btn2Pessed(){
+  answerNumber = 2;
+  checkRightAnswer()
+}
+
+function Btn3Pessed(){
+  answerNumber = 3;
+  checkRightAnswer()
+}
+
+function Btn4Pessed(){
+  answerNumber = 4;
+  checkRightAnswer()
+}
+
+
+
+function checkRightAnswer(){
    if (answerNumber === myQuestions[i].correctAnswer){
-       writeWrong.textContent = "Good Job - Corect"
+       writeWrong.textContent = "Good Job - Correct"
    }else{
-       writeWrong.textContent = "Not Quite"
+       writeWrong.textContent = "Not Quite - The correct answer is: " 
+                              + myQuestions[i].answers[myQuestions[i].correctAnswer];
        timeLeft = timeLeft -10
    }
-   writeWrong.style.display="block";
-   nextBtn.style.display="block";
+   answer1Btn.disabled = true;
+   answer2Btn.disabled = true;
+   answer3Btn.disabled = true;
+   answer4Btn.disabled = true;
+   writeWrong.style.display="initial";
+   nextBtn.style.display="initial";
 }
 
 // FUNTION TIME IS UP
   function timeIsUp(){
     questionNav.style.display="none";
-    quizEnd.style.display="block";
+    quizEnd.style.display="initial";
     nextBtn.style.display="none";
   }
 
@@ -142,6 +171,11 @@ init();
 
 //add event listener for start button
 startBtn.addEventListener("click", startQuiz);
-nextBtn.addEventListener("click", diplayQuestion);
+nextBtn.addEventListener("click", displayQuestion);
+answer1Btn.addEventListener("click", Btn1Pessed); 
+answer2Btn.addEventListener("click", Btn2Pessed); 
+answer3Btn.addEventListener("click", Btn3Pessed); 
+answer4Btn.addEventListener("click", Btn4Pessed); 
+
 
 
